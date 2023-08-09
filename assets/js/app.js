e6292de4cf618d16a06c2d6d9ac7b739b0f0302a
credtei <138682095+credtei@users.noreply.github.com>
2023-08-09 14:31:10 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,16 @@
 function menu() {
   if (window.innerWidth < 860) {
     var x = document.getElementById("myTopnav");
+    var y = document.getElementById("botao");
     var opa = document.getElementById("opaco-id");
-    if (x.className === "topnav") {
+    if (x.className === "topnav" && y.className === "fa fa-bars") {
       x.className += " responsive";
+      y.className += "fa fa-close";
       opa.className = "opaco responsive";
     } else {
       x.className = "topnav";
-      opa.className = "opaco";
-    }
-    var y = document.getElementById("botao");
-    if (y.className === "fa fa-bars"){
-      y.className += "fa fa-close";
-    } else {
       y.className = "fa fa-bars";
+      opa.className = "opaco";
     }
   }
 
@@ -45,9 +42,49 @@ function popUp(id) {
   if(opa.className === "opaco responsive"){
     opa.addEventListener("click", function(){
       if(pop.className === "popuptext show"){
-        pop.classList.toggle("show");
-        opa.className = "opaco";
+        function fechaPopUp(){
+          return new Promise(pop.classList.toggle("show"));
+        }
+        async function desativaOpaco(){
+          var x = await fechaPopUp();
+          opa.className = "opaco";
+        }
+        
       }
     })
   }
 }
+
+/*function popUp(id) {
+  var pop = document.getElementById(id);
+  if (pop.className === "popuptext"){
+    pop.className += " show";
+  } else {
+    pop.className = "popuptext";
+  }
+  var opa = document.getElementById("opaco-id");
+  if (opa.className === "opaco"){
+    opa.className += " responsive";
+  } else{
+    opa.className = "opaco";
+    pop.className = "popuptext";
+    opa.addEventListener("click", function(){
+      pop.className = "popuptext";
+      opa.className = "opaco";
+    })
+  }
+}
+
+
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+*/
+
